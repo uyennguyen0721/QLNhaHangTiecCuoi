3729ffdd617e6bdce2f88610d354ba4a6d405068
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-08-05 23:55:18 +0700

    index page

--- NhaHangTiecCuoi/NhaHangTiecCuoiApp/views.py
+++ NhaHangTiecCuoi/NhaHangTiecCuoiApp/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, template_name='index.html', context={
-        'name': 'Uyên Nguyễn'
+        'name': 'Trang chủ'
     })
